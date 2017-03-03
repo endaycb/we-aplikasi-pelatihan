88064d4b5f5109e904895b281ab65aa4be1cd16e
Mashiro Shiina <endaycb@gmail.com>
2017-03-03 18:34:50 +0700

    add getter setter

--- src/main/java/com/endaycb/belajar/model/Peserta.java
+++ src/main/java/com/endaycb/belajar/model/Peserta.java
@@ -46,5 +46,55 @@ public class Peserta {
     @ManyToOne
     @JoinColumn(name = "id_institusi")
     private Institusi institusi;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public Institusi getInstitusi() {
+        return institusi;
+    }
+
+    public void setInstitusi(Institusi institusi) {
+        this.institusi = institusi;
+    }
+    
+    
    
 }
